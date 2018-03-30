631721ba7240930397b308b55be05c5ea1772093
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-03-30 11:51:16 -0600

    saving and loading cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,4 +26,14 @@ defmodule Cards do
     deck = Cards.shuffle(deck)
     Enum.split(deck, hand_size)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {_, bin_data} = File.read(filename)
+    :erlang.binary_to_term(bin_data)
+  end
 end
