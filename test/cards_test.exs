c50971179fc7574a629e9fd23a83b6ec92b6551e
Baruc Almaguer <baruc.almaguer@gmail.com>
2018-03-31 20:48:40 -0600

    corrected cards_test (no hello function)

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,7 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "The truth" do
+    assert 1 + 1 == 2
   end
 end
